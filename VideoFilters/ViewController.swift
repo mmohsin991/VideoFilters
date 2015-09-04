3ed297a52ac3ee8c8768ce61ff27c7b96a534d06
Mohsin <muhammad.mohsin.991@gmail.com>
2015-09-04 16:50:27 +0500

    Initial Commit

---
+++ VideoFilters/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoFilters
+//
+//  Created by Mohsin on 04/09/2015.
+//  Copyright (c) 2015 Mohsin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
